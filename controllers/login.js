bc50ef64238c1210a4e6557bbb7a995fadbf1372
cmmurgo <cmmurgo@gmail.com>
2024-11-21 19:32:25 -0300

    finales, espero

--- controllers/login.js
+++ controllers/login.js
@@ -6,9 +6,8 @@ const User = require('../models/user');
 // Define la ruta POST para la autenticación de login
 loginRouter.post('/', async (request, response) => {
   
-  // Extrae el cuerpo de la solicitud (nombre de usuario y contraseña) de la petición
   const body = request.body;
-  
+
   // Busca en la base de datos un usuario con el nombre de usuario proporcionado
   const user = await User.findOne({ username: body.username });
   
@@ -20,7 +19,7 @@ loginRouter.post('/', async (request, response) => {
   // Si el usuario no existe o la contraseña es incorrecta, se responde con un error 401 (Unauthorized)
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: 'invalid username or password' // Mensaje de error si el usuario o la contraseña son incorrectos
+      error: 'username o password inválidos' 
     });
   }
 
@@ -33,7 +32,7 @@ loginRouter.post('/', async (request, response) => {
   // Genera un token JWT utilizando la clave secreta almacenada en las variables de entorno
   const token = jwt.sign(userForToken, process.env.SECRET); // El token se firma con los datos del usuario y la clave secreta
 
-  // Envía una respuesta con el token generado, junto con el nombre de usuario y el nombre completo del usuario
+  // Envía una respuesta con el token generado
   response
     .status(200) 
     .send({ token, username: user.username, name: user.name }); 
