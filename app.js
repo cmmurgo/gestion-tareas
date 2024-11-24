80b4e739f27055c280bdb09d5b0e850256d68e83
danielriveros07 <danielriveros07@gmail.com>
2024-11-23 22:27:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-require('dotenv').config(); // Carga variables de entorno desde un archivo .env
-const path = require('path'); 
-const tareasRoutes = require('./routes/tareas');
+const express = require('express');
+const path = require('path');
 const session = require('express-session');
+const mongoose = require('mongoose'); // Importar mongoose
+require('dotenv').config(); // Carga variables de entorno desde un archivo .env
+const connectDB = require('./config/database'); // Importar la función de conexión a la base de datos
+const tareasRoutes = require('./routes/tareas');
 
 // Importar controladores y middlewares
-const viewsController = require('./controllers/viewsController'); 
-const usersRouter = require('./controllers/users'); 
-const tareaTokenRouter = require('./controllers/tareas'); 
-const loginRouter = require('./controllers/login'); 
-const { errorHandler, authenticateToken } = require('./middlewares/middleware'); 
+const viewsController = require('./controllers/viewsController');
+const usersRouter = require('./controllers/users');
+const tareaTokenRouter = require('./controllers/tareas');
+const loginRouter = require('./controllers/login');
+const { errorHandler, authenticateToken } = require('./middlewares/middleware');
 
 // Configuración de la aplicación
-const app = express(); 
-app.use(express.static(path.join(__dirname, 'public'))); // Sirve archivos estáticos desde la carpeta 'public'
-mongoose.set('strictQuery', false); 
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+mongoose.set('strictQuery', false); // Asegurarse de que mongoose esté definido
+
+// Conectar a la base de datos (solo si no estamos en un entorno de pruebas)
+if (process.env.NODE_ENV !== 'test') {
+    connectDB();
+}
 
 // Configuración de sesiones
 app.use(session({
-    secret: 'codecrafters', 
+    secret: process.env.SECRET || 'orion123',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } 
+    cookie: { secure: false }
 }));
 
 // Ruta para cerrar sesión
@@ -32,32 +38,25 @@ app.get('/logout', (req, res) => {
     });
 });
 
-// Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tareasDB', { 
-    useNewUrlParser: true, // Configura Mongoose para usar el nuevo analizador de URL de MongoDB
-    useUnifiedTopology: true // Configura Mongoose para usar el nuevo motor de administración de conexiones
-})
-.then(() => console.log('Conectado a la base de datos')) 
-.catch(err => console.error('Error de conexión a la base de datos:', err)); 
-
 // Middleware para analizar datos en formato URL-encoded
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//vercel
+
+// Configuración de vistas
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug'); 
+app.set('view engine', 'pug');
 
 // Rutas para vistas
-app.get('/', viewsController.getHome); 
-app.get('/login', viewsController.getLogin); 
-app.get('/register', viewsController.getRegister); 
-app.get('/menu', viewsController.getMenu); 
+app.get('/', viewsController.getHome);
+app.get('/login', viewsController.getLogin);
+app.get('/register', viewsController.getRegister);
+app.get('/menu', viewsController.getMenu);
 
 // Rutas para la API (protegidas con el token)
-app.use('/api/users', usersRouter); // Rutas de la API de usuarios (sin autenticación)
-app.use('/api/menu', authenticateToken, tareaTokenRouter.crearTareaConToken); // Rutas de la API de tareas, protegidas con token
-app.use('/api/login', loginRouter); // Ruta de la API para autenticación (inicio de sesión)
-app.use(errorHandler); // Middleware global para manejo de errores
+app.use('/api/users', usersRouter);
+app.use('/api/menu', authenticateToken, tareaTokenRouter.crearTareaConToken);
+app.use('/api/login', loginRouter);
+app.use(errorHandler);
 
 // Montar las rutas de tareas con el prefijo /tareas
 app.use('/tareas', tareasRoutes);
@@ -67,7 +66,6 @@ app.use('/api', tareasRoutes);
 
 // Iniciar el servidor - verifica si el archivo app.js se está ejecutando directamente -node app.js o npm run dev
 // o si está siendo usado como módulo (como lo hace Vercel). 
-
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    testPathIgnorePatterns: ['/node_modules/'],
+  };
+  
